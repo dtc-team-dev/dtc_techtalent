3d44e9e8ac236499109688ebd7c623dd2f17fa4c
AndrhyExKobe <andri_dwiutomo@ymail.com>
2016-01-13 20:38:45 -0800

    BE - coba mengerti basic path yang sudah ada di server.js untuk node

--- config/main.js
+++ config/main.js
@@ -1,5 +1,5 @@
 module.exports = {
     "database"  : "mongodb://localhost:27017/touch_talent",
-    "port"      : process.env.PORT || 3000,
+    "port"      : process.env.PORT || 80,
     "secretKey" : "DTCSecretKey"
 }
