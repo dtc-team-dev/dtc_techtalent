b83a400dbcfa60fbbc5cd41f0b8ef573580210c6
Gifa Eriyanto <gifaeriyanto@ymail.com>
2016-01-14 14:02:15 +0800

    FE - bersih-bersih

--- public/assets/js/vendor.js
+++ public/assets/js/vendor.js
@@ -13493,3 +13493,11 @@ window.Modernizr = (function( window, document, undefined ) {
 }).call(this);
 ;// WOW
 new WOW().init();
+
+
+// Canvas
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(20,15);
+ctx.lineTo(300,100);
+ctx.stroke();
--- public/assets/src/js/custom.js
+++ public/assets/src/js/custom.js
@@ -1,2 +1,10 @@
 // WOW
 new WOW().init();
+
+
+// Canvas
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(20,15);
+ctx.lineTo(300,100);
+ctx.stroke();
