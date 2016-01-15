751beefad1801bdc6d2c574ab0c0916483e5bc24
Gifa Eriyanto <gifaeriyanto@ymail.com>
2016-01-15 19:21:59 +0800

    FE - style

--- config/main.js
+++ config/main.js
@@ -1,5 +1,5 @@
 module.exports = {
     "database"  : "mongodb://localhost:27017/tech_talent",
-    "port"      : process.env.PORT || 3000,
+    "port"      : process.env.PORT || 80,
     "secretKey" : "DTCSecretKey"
 }
