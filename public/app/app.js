af823318f9cb7b97e8b2dd463ac281e979bfc740
Gifa Eriyanto <gifaeriyanto@ymail.com>
2016-01-15 17:09:30 +0800

    FE - Remove Hash

--- public/app/app.js
+++ public/app/app.js
@@ -3,7 +3,7 @@ var app = angular.module("techTalentAsia", [
 	'ngSanitize'
 ]);
 
-app.config(function ($routeProvider) {
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 	$routeProvider
 		.when('/', {
 			controller: 'SiteController',
@@ -16,4 +16,6 @@ app.config(function ($routeProvider) {
 		.otherwise({
 			redirectTo: '/'
 		});
-});
+
+	$locationProvider.html5Mode(true);
+}]);
