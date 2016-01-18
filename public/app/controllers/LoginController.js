1d3db36bd7378375c179a3d615b780f49b3bc35e
hansenmakangiras <hansenmakangiras@gmail.com>
2016-01-18 14:13:09 +0800

    initial commit

--- public/app/controllers/LoginController.js
+++ public/app/controllers/LoginController.js
@@ -39,7 +39,7 @@ app.controller('LoginController', function($rootScope, $location, $window, Login
 		$window.location.reload();
 	};
 
-	$scope.authenticate = function(provider) {
+	vm.authenticate = function(provider) {
 		$auth.authenticate(provider);
 	};
 
@@ -52,7 +52,7 @@ app.controller('LoginController', function($rootScope, $location, $window, Login
 			var go = base + "/#" + url;
 			console.log(go);
 			// $window.location.href = go;
-		};
-	// });
+        }
+    // });
 
 });
